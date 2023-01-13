569086a9362746546655782068ce0e6756568db7
lvas463 <lvas463@gmail.com>
2023-01-13 10:56:55 -0500

    Transfer to another computer.

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,6 +1,34 @@
 use std::fs::File;
 use std::io::Read;
 
+enum MapError {
+    HouseDne
+}
+
+struct Coord {
+    x: i32,
+    y: i32
+}
+
+struct HouseMap {
+    origin: Coord, // placeholder
+    arr: Vec<Vec<i32>>
+}
+
+impl HouseMap {
+    fn new() -> HouseMap {
+        let temp_vec: Vec<Vec<i32>> = Vec::from(Vec::new());
+        HouseMap {
+            origin: Coord { x: 0, y: 0 },
+            arr: temp_vec
+        }
+    }
+    
+    fn push(self, x: i32, y: i32) -> Result<(), MapError> {
+        
+        Ok(())
+    }
+}
 
 pub fn directions() {
     let mut file = if let Ok(file) = File::open("./txt/day_3.txt") {
@@ -14,4 +42,8 @@ pub fn directions() {
         Ok(_) => println!("File read successfully."),
         Err(e) => panic!("Error: {:?}", e)
     }
+    
+    // Do Vectors expand automatically?
+    
+    
 }
