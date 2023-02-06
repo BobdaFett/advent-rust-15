ed7a8b897c56bd5ff38d10adb0743afd8e15131f
BobdaFett <lvas463@gmail.com>
2023-02-05 23:18:59 -0500

    Added rand crate to project.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 //
 
 use std::io::{stdin, stdout, Write};
+use rand::{thread_rng, Rng};
 
 pub mod day_1;
 pub mod day_2;
@@ -16,10 +17,10 @@ fn main() {
     let _ = stdout().flush();
     stdin().read_line(&mut user_in).unwrap();
 
-    if let Some('\n')=user_in.chars().next_back() {
+    if let Some('\n') = user_in.chars().next_back() {
         user_in.pop();
     }
-    if let Some('\r')=user_in.chars().next_back() {
+    if let Some('\r') = user_in.chars().next_back() {
         user_in.pop();
     }
     
@@ -29,7 +30,12 @@ fn main() {
         3 => day_3::directions(),
         4 => day_4::advent_coins(),
         5 => day_5::naughty_strings(),
-        100 => day_4::get_md5(String::from("abcdef")),
+        100 => hundred_nine(),
         other => panic!("Value {} is not a valid/developed day.", other)
     }
+}
+
+fn hundred_nine() {
+    print!("{}", thread_rng().gen_range(1..=1008));
+    println!(" ");
 }
